33842edba745fdd351d1652c27e43aafdcc925d6
Glytch3r <glytch3r.arts@gmail.com>
2025-04-13 09:31:20 +0800

    added the items and the quessy effect

--- media/lua/client/FoodGames_Queezy.lua
+++ media/lua/client/FoodGames_Queezy.lua
@@ -25,98 +25,61 @@
    ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
 █████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
 
-
------------------------            ---------------------------
 FoodGames = FoodGames or {}
-local Commands = {};
-Commands.FoodGames = {};
-
---print(getPlayer():HasTrait("HomeLender") )
-FoodGames.hitReactList ={
-    ["BEHIND"]="Zed_CatapultedBehindBack",
-    ["LEFT"]="Zed_CatapultedLeftFront",
-    ["RIGHT"]="Zed_CatapultedRightFront",
-    ["FRONT"]="Zed_CatapultedFrontFront",
-}
-
-function FoodGames.Catapult(zed, pl, bodyPartType, wpn)
-    if not zed or not pl then return end
-	if not pl:HasTrait("HomeLender") then return end
-	zed:setAvoidDamage(true)
-    local pos = zed:getPlayerAttackPosition()
-    -- if not  pl:getModData()['FoodGames']['Catapult'] then return end
-    if isClient() then
-        sendClientCommand('FoodGames', 'Catapult', {zedID = zed:getOnlineID(), pos = pos})
-    else
-        FoodGames.doPush(zed, pos)
-    end
-    if getCore():getDebug()then
-        print(tostring(pos))
-        zed:addLineChatElement(tostring(pos))
-        local react = (FoodGames.hitReactList[pos])
-        if react then
-            pl:addLineChatElement(tostring(pos))
-        end
-	end
-    local ShoveKills = SandboxVars.FoodGames.ShoveKills or true
-    if ShoveKills then
-        timer:Simple(3, function()
-        	zed:setAvoidDamage(false)
-            zed:changeState(ZombieOnGroundState.instance())
-            --zed:setAttackedBy(getCell():getFakeZombieForHit())
-            zed:setAttackedBy(pl)
-            zed:becomeCorpse()
-        end)
-    end
-
-
-end
-Events.OnHitZombie.Remove(FoodGames.Catapult)
-Events.OnHitZombie.Add(FoodGames.Catapult)
-
-function FoodGames.findzedID(int)
-	local zombies = getCell():getObjectList()
-	for i=zombies:size(),1,-1 do
-		local zed = zombies:get(i-1)
-		if instanceof(zed, "IsoZombie") then
-			local zedID=zed:getOnlineID()
-			if zedID and zedID == int then return zed end
-		end
-	end
-	return nil
-end
-
-Commands.FoodGames.Catapult = function(args)
-    local zedID = args.zedID
-    local pos = args.pos
-    if type(zedID) == 'string' then zedID = tonumber(zedID) end
-    local zed = FoodGames.findzedID(zedID)
-    if zed then
-        FoodGames.doPush(zed, pos)
-    end
-end
-
-Events.OnServerCommand.Add(function(module, command, args)
-	if Commands[module] and Commands[module][command] then
-		Commands[module][command](args)
-	end
-end)
-
-function FoodGames.doPush(zed, pos)
-    if not zed then return end
-    if not pos then return end
-    --local isBehind = zed:getPlayerAttackPosition() == 'BEHIND' or false
-    local react = (FoodGames.hitReactList[pos])
-    if not react then return end
-    zed:setHitReaction("Zed_CatapultedRightFront")
-    zed:setPlayerAttackPosition(pos)
-    zed:setHitReaction(react)
-    zed:setKnockedDown(true)
-end
---[[
-FoodGames.dbg(dbgZed, true)
-FoodGames.dbg(dbgZed, false)
- ]]
-
 -----------------------            ---------------------------
 
+function FoodGames.setQueezy(bool)
+    local pl = getPlayer()
+    local stats = pl:getStats()
+    local bd = pl:getBodyDamage()
+    if bool then
+        if stats:getSickness() ~= 1 then
+            stats:setSickness(1)
+        end
+
+        if bd:getFoodSicknessLevel() ~= 100 then
+            bd:setFoodSicknessLevel(100)
+        end
+    end
+
+end
+
+function FoodGames.removeQueezy()
+    local pl = getPlayer()
+    local stats = pl:getStats()
+    local bd = pl:getBodyDamage()
+
+    if stats:getSickness() > 0 then
+        stats:setSickness(0)
+    end
+
+    if bd:getFoodSicknessLevel() > 0 then
+        bd:setFoodSicknessLevel(0)
+    end
+end
+
+function FoodGames.updateQueezyHr()
+    local pl = getPlayer()
+    local curTime = getGameTime():getWorldAgeHours()
+    local targTime = 24
+    local md = pl:getModData()
+    if not md['QueezyHr'] then return end
+    md['QueezyHr'] = md['QueezyHr']  - 1
+    if md['QueezyHr'] == 0 then
+        md['QueezyHr'] = nil
+        FoodGames.removeQueezy()
+    end
+    if not md['QueezyHr'] then
+        FoodGames.removeQueezy()
+    end
+end
+
+Events.EveryHours.Add(FoodGames.updateQueezyHr)
+
+function FoodGames.Dizzy(pl)
+    local md = pl:getModData()
+    if not md then return end
+    if not md['QueezyHr'] then return end
+    FoodGames.setQueezy(true)
+end
+Events.OnPlayerUpdate.Add(FoodGames.Dizzy)
--- media/lua/server/FoodGames_Items.lua
+++ media/lua/server/FoodGames_Items.lua
@@ -28,95 +28,18 @@
 
 -----------------------            ---------------------------
 FoodGames = FoodGames or {}
-local Commands = {};
-Commands.FoodGames = {};
 
---print(getPlayer():HasTrait("HomeLender") )
-FoodGames.hitReactList ={
-    ["BEHIND"]="Zed_CatapultedBehindBack",
-    ["LEFT"]="Zed_CatapultedLeftFront",
-    ["RIGHT"]="Zed_CatapultedRightFront",
-    ["FRONT"]="Zed_CatapultedFrontFront",
-}
+function FoodGames.ZedItemDropHandler()
+      local qty =  SandboxVars.FoodGames.MaxZedDrops or 5
+      local inv = zed:getInventory();
+      inv:AddItems("Base.ZeeVeeMutagen",  ZombRand(0, qty + 1))
 
-function FoodGames.Catapult(zed, pl, bodyPartType, wpn)
-    if not zed or not pl then return end
-	if not pl:HasTrait("HomeLender") then return end
-	zed:setAvoidDamage(true)
-    local pos = zed:getPlayerAttackPosition()
-    -- if not  pl:getModData()['FoodGames']['Catapult'] then return end
-    if isClient() then
-        sendClientCommand('FoodGames', 'Catapult', {zedID = zed:getOnlineID(), pos = pos})
-    else
-        FoodGames.doPush(zed, pos)
-    end
-    if getCore():getDebug()then
-        print(tostring(pos))
-        zed:addLineChatElement(tostring(pos))
-        local react = (FoodGames.hitReactList[pos])
-        if react then
-            pl:addLineChatElement(tostring(pos))
-        end
-	end
-    local ShoveKills = SandboxVars.FoodGames.ShoveKills or true
-    if ShoveKills then
-        timer:Simple(3, function()
-        	zed:setAvoidDamage(false)
-            zed:changeState(ZombieOnGroundState.instance())
-            --zed:setAttackedBy(getCell():getFakeZombieForHit())
-            zed:setAttackedBy(pl)
-            zed:becomeCorpse()
-        end)
-    end
-
-
-end
-Events.OnHitZombie.Remove(FoodGames.Catapult)
-Events.OnHitZombie.Add(FoodGames.Catapult)
-
-function FoodGames.findzedID(int)
-	local zombies = getCell():getObjectList()
-	for i=zombies:size(),1,-1 do
-		local zed = zombies:get(i-1)
-		if instanceof(zed, "IsoZombie") then
-			local zedID=zed:getOnlineID()
-			if zedID and zedID == int then return zed end
-		end
-	end
-	return nil
-end
-
-Commands.FoodGames.Catapult = function(args)
-    local zedID = args.zedID
-    local pos = args.pos
-    if type(zedID) == 'string' then zedID = tonumber(zedID) end
-    local zed = FoodGames.findzedID(zedID)
-    if zed then
-        FoodGames.doPush(zed, pos)
     end
 end
 
-Events.OnServerCommand.Add(function(module, command, args)
-	if Commands[module] and Commands[module][command] then
-		Commands[module][command](args)
-	end
-end)
+Events.OnZombieDead.Add(FoodGames.ZedItemDropHandler);
 
-function FoodGames.doPush(zed, pos)
-    if not zed then return end
-    if not pos then return end
-    --local isBehind = zed:getPlayerAttackPosition() == 'BEHIND' or false
-    local react = (FoodGames.hitReactList[pos])
-    if not react then return end
-    zed:setHitReaction("Zed_CatapultedRightFront")
-    zed:setPlayerAttackPosition(pos)
-    zed:setHitReaction(react)
-    zed:setKnockedDown(true)
+function Recipesa.OnCreate.ZVMutagen(items, result, player)
+   local dur = SandboxVars.FoodGames.QueezyDuration
+   player:getModData()['QueezyHr'] = tonumber(dur)
 end
---[[
-FoodGames.dbg(dbgZed, true)
-FoodGames.dbg(dbgZed, false)
- ]]
-
------------------------            ---------------------------
-
