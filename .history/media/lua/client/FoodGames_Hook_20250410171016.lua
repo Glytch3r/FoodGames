bec8591fe801303e3170b9086b9389e01f19ea90
Glytch3r <glytch3r.arts@gmail.com>
2025-05-03 19:21:43 +0800

    cleared history

--- .history/media/lua/client/FoodGames_Hook_20250410171016.lua
+++
@@ -1,9 +0,0 @@
-FoodGames = FoodGames or {}
-
-local hook = ISEatFoodAction.start
-function ISEatFoodAction:start()
-    hook(self)
-    self:handleFoodGamesCaloriesOnStart()
-	self.character:reportEvent("EventEating")
-end
-
