e5bbf09b107e5cdc87a11a7c1c17b628b96ff5ec
Glytch3r <glytch3r.arts@gmail.com>
2025-05-05 02:38:42 +0800

    added sandbox option  to toggle heal notifications

--- media/lua/client/FoodGames_Heal.lua
+++ media/lua/client/FoodGames_Heal.lua
@@ -159,16 +159,20 @@ function FoodGames.HealRandPart(pl)
 
 
 
-
-
-
     end
-
-    if #healable == 0 then return false end
+    local isShowAlert = SandboxVars.FoodGames.InjuryHealOverheadMessage or true
+    if #healable == 0 then
+        return false
+    end
 
     local injury = healable[ZombRand(#healable) + 1]
     injury.func(injury.part)
-    print(string.format("[FoodGames] Healed %s on %s", injury.label, tostring(injury.part:getType())))
+
+    if isShowAlert then
+        local msg = string.format("[FoodGames] Healed %s on %s", injury.label, tostring(injury.part:getType()))
+        print(msg)
+        pl:addLineChatElement(tostring(msg))
+    end
     return true
 end
 
@@ -176,68 +180,3 @@ end
 
 
 
-
-
---[[
-function FoodGames.HealRandPart(pl)
-    local healable = {}
-
-    pl = pl or getPlayer()
-    local bodyDamage = pl:getBodyDamage()
-    local bodyParts = bodyDamage:getBodyParts()
-    for i=1, bodyParts:size() do
-        local part = bodyParts:get(i-1)
-
-        if part.scratched        and  part:getScratchTime() > 0         then   table.insert(healable, { part=part, label="Scratched",      func=function(p)
-            p:setScratched(false, true)
-            p:setScratchTime(0
-        )end }) end
-        if part.isCut            and    part:isCut()             then   table.insert(healable, { part=part, label="Cut",            func=function(p)
-            p:setCut(false)
-            p:setCutTime(0)
-        end }) end
-        if part.isDeepWounded      and    part:isDeepWounded()  and part:getDeepWoundTime() > 0 then   table.insert(healable, { part=part, label="DeepWounded",    func=function(p)
-            p:setDeepWoundTime(0)
-            p:setDeepWounded(false)
-            p:setBleedingTime(0)
-        end }) end
-        if part.bitten           and    part:bitten()            then   table.insert(healable, { part=part, label="Bitten",         func=function(p)
-            p:SetBitten(false);
-            p:SetInfected(false);
-            p:SetFakeInfected(false);
-        end }) end
-        if part.bleeding         and    part:bleeding()          then   table.insert(healable, { part=part, label="Bleeding",       func=function(p)
-            p:setBleedingTime((p:getBleedingTime() > 0) and 0 or 10)
-        end }) end
-        if part.isInfectedWound  and    part:isInfectedWound()   then   table.insert(healable, { part=part, label="InfectedWound",  func=function(p)
-            p:setWoundInfectionLevel(-1)
-        end }) end
-        if part.haveBullet       and    part:haveBullet()        then   table.insert(healable, { part=part, label="Have Bullet",     func=function(p)
-            local deepWound = p:isDeepWounded()
-            local deepWoundTime = p:getDeepWoundTime()
-            local bleedTime = p:getBleedingTime()
-            p:setHaveBullet(false, 0)
-            p:setDeepWoundTime(deepWoundTime)
-            p:setDeepWounded(deepWound)
-            p:setBleedingTime(bleedTime)
-        end }) end
-        if part.getFractureTime and  part:getFractureTime() > 0 then
-            table.insert(healable, { part=part, label="Fracture", func=function(p)
-                p:setFractureTime(0.0)
-            end })
-        end
-        if part.getBurnTime and part:getBurnTime() > 0 then
-            table.insert(healable, { part=part, label="Burn", func=function(p)
-                p:setNeedBurnWash(not p:isNeedBurnWash())
-            end })
-        end
-    end
-
-    if #healable == 0 then return false end
-
-    local injury = healable[ZombRand(#healable) + 1]
-    injury.func(injury.part)
-    print(string.format("[FoodGames] Healed %s on %s", injury.label, tostring(injury.part:getType())))
-    return true
-end
- ]]
